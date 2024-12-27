cfea4ce3b8ec8d31e83d34a06bd46dccd435b743
JayJayArr <JayArr@outlook.de>
2024-12-27 15:47:01 +0100

    feat: add ASCII conversion

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
+use std::char;
+
 use clap::Parser;
 
-/// Simple program to convert a Cardnumber into all kinds of formats to help with creating a card format
+/// Convert a Cardnumber into all kinds of formats to help with creating a card format
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     /// cardno in DEC format that shall be converted
     cardno: i128,
+
+    /// optional: Data in which the cardnumber should be found
+    data: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
     let declength = args.cardno.to_string().len();
+    let chars: Vec<char> = args.cardno.to_string().chars().collect();
+    // if let Some(haystack) = args.data.as_deref() {
+    //     println!("{:?}", args.data.as_deref());
+    //     chars = args.data.unwrap().chars().collect();
+    // }
+    let mut asciichars: Vec<u8> = vec![];
+    for char in chars {
+        asciichars.push(char as u8);
+    }
     let mut hexlength = format!("{:X}", args.cardno).to_string().len();
     let orighexlength = hexlength;
     if hexlength % 2 == 1 {
@@ -47,6 +61,15 @@ fn main() {
             .rev()
             .collect::<String>()
     );
+    println!(
+        "ASCII (lowercase): {}",
+        format_args!("{:?}", asciichars.to_ascii_lowercase())
+    );
+    println!(
+        "ASCII (uppercase): {}",
+        format_args!("{:?}", asciichars.to_ascii_uppercase())
+    );
+
     println!("Dec length: {}", format_args!("{:?}", declength));
     if hexlength != orighexlength {
         println!(
